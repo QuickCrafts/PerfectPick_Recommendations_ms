e26060cf6e694d0b237efea8a0e4da02b7ae6f23
dmriveroe <104804272+XdmriveroeX@users.noreply.github.com>
2024-03-09 17:33:09 -0500

    Recommendation APIs

--- routes/route.py
+++ routes/route.py
@@ -1,43 +1,57 @@
 from fastapi import APIRouter
-from models.recommendations import Recommendation
+from models.recommendations import ItemRemovalModel, RecommendationModel, RecommendationUpdateModel
 from config.database import collection_name
 from schema.schemas import list_serial
 from bson import ObjectId
+from fastapi import HTTPException
 
 router = APIRouter()
 
-# GET Request Method
-@router.get("/")
-async def get_all_recommendations():
-    recommendations = list_serial(collection_name.find())
-    return recommendations
+# GET Request Method for specific user's recommendation
+@router.get("/recommendation/{id_user}")
+async def get_recommendation_for_user(id_user: int):
+    recommendation = list_serial(collection_name.find({"id_user": id_user}))
+    if not recommendation:
+        raise HTTPException(status_code=404, detail="Recommendation not found")
+    return recommendation
 
 
-# POST Request Method
-@router.post("/")
-async def post_recommendation(recommendation: Recommendation):
+# POST Request Method to create a new recommendation
+@router.post("/recommendation/")
+async def create_recommendation(recommendation: RecommendationModel):
     collection_name.insert_one(dict(recommendation))
     return {
         "data": "recommendation added successfully"
     }
 
-""" 
 
-# PUT Request Method
-@router.put("/{id}")
-async def update_recommendation(id: str, recommendation: R):
-    collection_name.update_one({"_id": ObjectId(id)}, {"$set": dict(recommendation)})
-    return {
-        "data": "recommendation updated successfully"
-    }
- 
+# PUT Request Method to update a recommendation for a specific user
+@router.put("/recommendation/{id_user}")
+async def update_recommendation_for_user(id_user: int, recommendation: RecommendationUpdateModel):
+    updated_recommendation = {k: v for k, v in recommendation.model_dump(exclude_unset=True).items() if v is not None}
+    collection_name.update_one({"id_user": id_user}, {"$set": updated_recommendation})
+    return {"data": "Recommendation updated successfully"}
+
 
 # DELETE Request Method
-@router.delete("/{id}")
-async def delete_recommendation(id: str):
-    collection_name.delete_one({"_id": ObjectId(id)})
+@router.delete("/recommendation/{id_user}")
+async def delete_recommendation(id_user: int):
+    collection_name.delete_one({"id_user": id_user})
     return {
         "data": "recommendation deleted successfully"
     }
 
- """
+@router.put("/recommendation/remove/{id_user}")
+async def remove_item_from_recommendation(id_user: int, removal_info: ItemRemovalModel):
+    if removal_info.section not in ["movies", "books", "songs"]:
+        raise HTTPException(status_code=400, detail="Invalid section specified")
+
+    update_result = collection_name.update_one(
+        {"id_user": id_user},
+        {"$pull": {removal_info.section: removal_info.id_to_remove}}
+    )
+
+    if update_result.modified_count == 0:
+        raise HTTPException(status_code=404, detail="User not found or item not in list")
+    
+    return {"message": "Item removed successfully"}
