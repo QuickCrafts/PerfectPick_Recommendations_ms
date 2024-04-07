d0f44bd2ce67603b1b29604e6355a7c385ace844
dmriveroe <104804272+XdmriveroeX@users.noreply.github.com>
2024-04-07 17:45:52 -0500

    mega docker

--- run_API.bat
+++ run_API.bat
@@ -8,16 +8,16 @@ rem Delete existing recommendation-ms images
 docker rmi recommendation-ms
 
 rem Create network
-docker network create mynetwork
+docker network create perfectpicknetwork
 
 rem Create RabbitMQ container
-docker run -d --rm --name rabbitmq --network mynetwork -p 5672:5672 -p 15672:15672 rabbitmq
+docker run -d --rm --name rabbitmq --network perfectpicknetwork -p 5672:5672 -p 15672:15672 rabbitmq
 
 rem Create MongoDB container
-docker run -d --name mongo --network mynetwork -p 27017:27017 -v mongodbdata:/data/db mongo
+docker run -d --name mongo --network perfectpicknetwork -p 27017:27017 -v mongodbdata:/data/db mongo
 
 rem Build recommendation-ms image
 docker build -t recommendation-ms .
 
 rem Create recommendation-ms container
-docker run -d --name recommendationms --network mynetwork -p 8000:8000 recommendation-ms
+docker run -d --name recommendationms --network perfectpicknetwork -p 8000:8000 recommendation-ms
